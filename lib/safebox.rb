3eeb46260fa19582e90e8316006243ec454f6649
Denis Knauf <deac+git@denkn.at>
2021-12-03 18:24:18 +0100

    files replaced by README.md to inform viewers, repository is moved to git.denkn.at.

--- lib/safebox.rb
+++
@@ -1,7 +0,0 @@
-
-raise Exception, 'Rubinius does not support $SAFE.  Safebox is useless.'  if Object.const_defined?( :RUBY_ENGINE) and 'rbx' == RUBY_ENGINE
-
-require 'safebox/safebox'
-require 'safebox/box'
-require 'safebox/emit'
-require 'safebox/persistent'
